1a3da4ca5bcb2c0591ded91841180069093458d7
Halid Cisse <halidoucse@gmail.com>
2016-05-05 10:25:34 +0100

    Initial Commit

---
+++ GitWatch/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GitWatch
+//
+//  Created by Halid Cisse on 5/5/16.
+//  Copyright © 2016 Halid Cisse. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
