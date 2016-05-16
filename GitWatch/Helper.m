162023c619bbb51290f69beb7ea9650beaed916c
Halid Cisse <halidoucse@gmail.com>
2016-05-16 17:24:04 +0100

    Minor

--- GitWatch/Helper.m
+++ GitWatch/Helper.m
@@ -11,6 +11,9 @@
 
 @implementation Helper
 
+
+
+
 + (void)ClearCredentials
 {
     [SSKeychain setPassword:@"" forService:@"GitHub.com" account:@"GitHub.com"];
@@ -34,6 +37,7 @@
     
     [SSKeychain setPassword:GitHubClient.token forService:@"GitHub.com" account:@"Token"];
 }
+
 
 + (BOOL)IsFavorite:(NSString *)repositoryName
 {
@@ -60,5 +64,47 @@
         return true;
     }
 }
+
+
++ (void)SaveRepoInterval:(NSString *)RepoName forDays: (NSNumber*) days
+{
+    NSString *destPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+    destPath = [destPath stringByAppendingPathComponent:@"RepositoryInterval.plist"];
+    
+    // If the file doesn't exist in the Documents Folder, copy it.
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    
+    if (![fileManager fileExistsAtPath:destPath]) {
+        NSString *sourcePath = [[NSBundle mainBundle] pathForResource:@"RepositoryInterval" ofType:@"plist"];
+        [fileManager copyItemAtPath:sourcePath toPath:destPath error:nil];
+    }
+    
+    NSMutableDictionary*plistDict=[[NSMutableDictionary alloc] initWithContentsOfFile:destPath];
+    //Manipulate the dictionary
+    //NSString *obj = [[[NSString alloc] initWithFormat:] ]
+    [plistDict setObject:days forKey:RepoName];
+    //Again save in doc directory.
+    [plistDict writeToFile:destPath atomically:YES];
+    
+    NSMutableArray *favoritesRepos = [[NSMutableArray alloc] initWithContentsOfFile:destPath];
+    
+    if (favoritesRepos == nil) {
+        favoritesRepos = [[NSMutableArray alloc] init];
+    }
+    
+    NSString *key =[[NSString alloc]initWithFormat:@"%@",RepoName];
+    
+//    if (sender.on) {
+//        if ([favoritesRepos indexOfObject:key] == NSNotFound) {
+//            [favoritesRepos addObject:key];
+//        }
+//    } else {
+//        if ([favoritesRepos indexOfObject:key] != NSNotFound) {
+//            [favoritesRepos removeObject:key];
+//        }
+//    }
+//    [favoritesRepos writeToFile:destPath atomically:YES];
+}
+
 
 @end
